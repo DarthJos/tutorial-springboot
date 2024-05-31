c5356f64afae9ced33ef4148d6069af694c7b2d9
Josimar Reyes Campos <darth_jos@hotmail.com>
2024-05-31 12:01:13 -0600

    Tutorial | Josi RC | Removed print() test method

--- src/main/java/com/jrprojects/tutorial/TutorialApplication.java
+++ src/main/java/com/jrprojects/tutorial/TutorialApplication.java
@@ -8,12 +8,5 @@ public class TutorialApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TutorialApplication.class, args);
-
-		print(1, 2);
 	}
-
-	public static void print(int num, int num2) {
-		System.out.println("Hola");
-	}
-
 }
