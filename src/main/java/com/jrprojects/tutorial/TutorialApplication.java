b564180b78a4aa5506992dda683a2e27367d8ae6
Josimar Reyes Campos <darth_jos@hotmail.com>
2024-06-04 07:54:16 -0600

    Tutorial | Josi RC | Added logger msgs and yml config

--- src/main/java/com/jrprojects/tutorial/TutorialApplication.java
+++ src/main/java/com/jrprojects/tutorial/TutorialApplication.java
@@ -1,12 +1,20 @@
 package com.jrprojects.tutorial;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TutorialApplication {
 
+	private static Logger logger = LoggerFactory.getLogger(TutorialApplication.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(TutorialApplication.class, args);
+
+		logger.debug("Mi mensaje debug");
+		logger.trace("Mi mensaje trace");
+		logger.info("Mi mensaje info");
 	}
 }
