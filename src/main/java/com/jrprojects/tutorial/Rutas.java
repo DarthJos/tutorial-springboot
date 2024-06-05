a9a61bc3e358e89f618a6a497dbda27809e4d64f
Josimar Reyes Campos <darth_jos@hotmail.com>
2024-06-04 23:23:35 -0600

    Tutorial | Josi RC | Add PostMapping method guardarLibro

--- src/main/java/com/jrprojects/tutorial/Rutas.java
+++ src/main/java/com/jrprojects/tutorial/Rutas.java
@@ -1,12 +1,16 @@
 package com.jrprojects.tutorial;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+import java.util.Objects;
 
 @RestController
 public class Rutas {
+
+    private final Logger logger = LoggerFactory.getLogger(Rutas.class);
 
     @GetMapping ("/hola")
     String miPrimeraRuta() {
@@ -21,5 +25,14 @@ public class Rutas {
     @GetMapping ("/libro2/{id}")
     String leerLibroParams(@PathVariable int id, @RequestParam  String param1, @RequestParam String editorial) {
         return "Leyendo el libro id: " + id + ", param1: " + param1 + ", editorial: "+ editorial;
+    }
+
+    @PostMapping ("/libro")
+    String guardarLibro(@RequestBody Map<String, Object> libro) {
+        libro.keySet().forEach(llave -> {
+            logger.debug("llave {} valor {}" , llave, libro.get(llave));
+        });
+
+        return "Libro guardado...";
     }
 }
