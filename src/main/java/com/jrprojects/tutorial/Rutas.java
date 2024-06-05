e9e79366f561b5f72fd5f620a4640c1e91f4f260
Josimar Reyes Campos <darth_jos@hotmail.com>
2024-06-04 21:34:03 -0600

    Tutorial | Josi RC | Add leerLibroParams method with RequestParam tags

--- src/main/java/com/jrprojects/tutorial/Rutas.java
+++ src/main/java/com/jrprojects/tutorial/Rutas.java
@@ -2,6 +2,7 @@ package com.jrprojects.tutorial;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -15,5 +16,10 @@ public class Rutas {
     @GetMapping ("/libro/{id}/editorial/{editorial}")
     String leerLibro(@PathVariable int id, @PathVariable String editorial) {
         return "Leyendo el libro id: " + id + ", editorial: " + editorial;
+    }
+
+    @GetMapping ("/libro2/{id}")
+    String leerLibroParams(@PathVariable int id, @RequestParam  String param1, @RequestParam String editorial) {
+        return "Leyendo el libro id: " + id + ", param1: " + param1 + ", editorial: "+ editorial;
     }
 }
