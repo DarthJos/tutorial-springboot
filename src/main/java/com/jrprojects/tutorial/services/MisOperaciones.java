ef8c567570c9bf5eba1730cd542d5eb56538f587
Josimar Reyes Campos <darth_jos@hotmail.com>
2024-06-12 09:16:37 -0600

    Tutorial | UnitTest | 23:00 | Add factorial success and fail test cases

--- src/main/java/com/jrprojects/tutorial/services/MisOperaciones.java
+++ src/main/java/com/jrprojects/tutorial/services/MisOperaciones.java
@@ -6,7 +6,12 @@ import org.springframework.stereotype.Service;
 public class MisOperaciones implements Operaciones{
     @Override
     public int factorial(int num) {
-        if (num == 1) return 1;
+
+        if (num < 0) throw new ArithmeticException();
+
+        if (num >23) throw new IllegalArgumentException("Overflow from integer, use max 23");
+
+        if (num == 1 || num == 0) return 1;
         return num * factorial(num -1 );
     }
 }
---
+++ src/test/java/services/OperacionesTest.java
@@ -0,0 +1,40 @@
+package services;
+
+import com.jrprojects.tutorial.services.MisOperaciones;
+import com.jrprojects.tutorial.services.Operaciones;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class OperacionesTest {
+
+    private Operaciones operaciones;
+
+    @BeforeEach
+    public void setup() {
+        operaciones = new MisOperaciones();
+    }
+
+    /**
+     * Success test Cases
+     */
+    @Test
+    public void factorial_Test() {
+        Assertions.assertEquals(operaciones.factorial(0), 1);
+        Assertions.assertEquals(operaciones.factorial(1), 1);
+        Assertions.assertEquals(operaciones.factorial(4), 24);
+        Assertions.assertEquals(operaciones.factorial(5), 120);
+    }
+
+    /**
+     * Fail Test Cases
+     */
+    @Test
+    public void factorial_FailTest() {
+        Assertions.assertThrows(ArithmeticException.class, () -> operaciones.factorial(-1));
+
+        Assertions.assertThrows(ArithmeticException.class, () -> operaciones.factorial(-10));
+
+        Assertions.assertThrows(IllegalArgumentException.class, () -> operaciones.factorial(24));
+    }
+}
